7bf63704545211fce32e1729d238bdc121c4bfc5
何年 <1447471676@qq.com>
2019-03-22 00:34:03 +0800

    何年 2019/3/22 0:33

---
+++ test/shaixuan.js
@@ -0,0 +1,20 @@
+var pArr = [
+    {
+        name : "Autumn",
+        age : 18,
+        sex : "男",
+        jianjie : "犯得上犯得上发生发射点发射点犯得上"
+    },
+    {
+        name : "小可",
+        age : 18,
+        sex : "女",
+        jianjie : "大美女 哈哈哈哈"
+    },
+    {
+        name : "杰瑞",
+        age : 18,
+        sex : "人妖",
+        jianjie : "萨瓦迪卡 乌西卡玛西狄俄"
+    }
+];
